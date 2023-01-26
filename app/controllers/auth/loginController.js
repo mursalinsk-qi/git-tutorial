bddb0ec54ba6bfbc803ac48d3f80e2ec77425153
mursalinsk310 <mursalinsk310@gmail.com>
2023-01-27 00:27:55 +0530

    login feature complete

---
+++ app/controllers/auth/loginController.js
@@ -0,0 +1,26 @@
+const User=require('../../models/user');
+
+exports.loginUser = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(409).json({
+            status: false,
+            message:'invalid email or password',
+        });
+    }
+    const matchPassword = await bcrypt.compare(password, user.password);
+    console.log(user);
+    if (!matchPassword) {
+        return res.status(409).json({
+            status: false,
+            message:'invalid email or password',
+        });
+    }
+    res.status(200).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+    })
+
+})
