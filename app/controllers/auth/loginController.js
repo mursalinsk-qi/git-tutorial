f752bf5246fefaeec209af4729b7eff6ec144ef0
Mursalin Sk <mursalin.sk@quantuminventions.com>
2023-01-27 09:40:31 +0530

    normal password store changed to hashed value

--- app/controllers/auth/loginController.js
+++ app/controllers/auth/loginController.js
@@ -1,5 +1,5 @@
 const User=require('../../models/user');
-
+const bcrypt=require('bcryptjs');
 exports.loginUser = async (req, res, next) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
